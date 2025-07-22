b24d9730b114439f850bfbb79e7e2b8b820592e7
taleb  kanj <talebkanj4@gmail.com>
2025-07-22 18:00:00 +0300

    تعريف هيكلية (Schema) ونموذج (Model) للمستخدمين والحوالات المالية

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,6 +1,7 @@
  
 const mongoose = require('mongoose');
 
+
 const transactionSchema = new mongoose.Schema({
   fromUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   toUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
